105d7a2594ec26a8e5e9b143d6631254fc3e2436
Quoc Hoang <nqh130901@gmail.com>
2022-12-14 17:15:21 +0700

    new update

--- testNextjs/myapp/pages/Marketplace/index.js
+++ testNextjs/myapp/pages/Marketplace/index.js
@@ -7,35 +7,37 @@ export default function Marketplace() {
     const urlApiEndpoit = "http://localhost:3000/api/getNftData-lib"
     const [nfts, setNfts] = useState([])
     const [type, setType] = useState()
-    
+
     useEffect(() => {
-        async function getData(){
+        async function getData() {
             const request = await fetch(urlApiEndpoit)
             const req = await request.json()
             setNfts(req.data)
         }
         getData()
-    },[])
+    }, [])
 
-  return (
-    <div className={styles.container}>
-        <div className="title is-size-5 p-4 mb-0">Marketplace</div>
-        <div className="columns is-multiline">
-            {nfts.map(item => {
-                return(
-                    <Link className="column is-3" href={item.contract_address + "/" + item.tokenid}>
-                        <div className={styles.nftcard}>
-                            <img src={item.image}/>
-                            <p className={styles.name}>{item.name}</p>
-                            <div className={styles.price}>
-                                <p>Price: {item.price}</p>
-                                {item['typecoin'] == 'BNB'? <Image src="/images/bnbcoin.png" width={15} height={17}/> : <Image src="/images/boilogo.png" width={15} height={17}/>}
+    return (
+        <div className={styles.container}>
+            <div className="title is-size-5 p-4 mb-0 has-text-white">Marketplace</div>
+            <div className="columns is-multiline">
+                {nfts.map(item => {
+                    return (
+                        <Link href={item.contract_address + "/" + item.tokenid}>
+                            <div className="column is-4">
+                                <div className={styles.nftcard}>
+                                    <img src={item.image} />
+                                    <p className={styles.name}>{item.name}</p>
+                                    <div className={styles.price}>
+                                        <p>Price: {item.price}</p>
+                                        {item['typecoin'] == 'BNB' ? <Image src="/images/bnbcoin.png" width={15} height={17} /> : <Image src="/images/boilogo.png" width={15} height={17} />}
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </Link>
-                )
-            })}
+                        </Link>
+                    )
+                })}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
